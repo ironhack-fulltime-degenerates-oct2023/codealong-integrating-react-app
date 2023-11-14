fb96df28972afe96dde995494bbd3cf77ba0ef61
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-11-14 16:39:18 +0100

    functionality to add new tasks

--- src/pages/ProjectDetailsPage.jsx
+++ src/pages/ProjectDetailsPage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link, useParams } from "react-router-dom";
+import AddTask from "../components/AddTask";
 
 const API_URL = "https://project-management-api-4641927fee65.herokuapp.com";
 
@@ -33,6 +34,11 @@ function ProjectDetailsPage(props) {
                     <p>{project.description}</p>
                 </>
             )}
+
+            <AddTask 
+                projectId={projectId} 
+                callbackToUpdateProject={getProject} 
+            />
 
             {project &&
                 project.tasks.map((task) => (
